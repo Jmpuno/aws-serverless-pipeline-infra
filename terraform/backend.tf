7268a178e75b52c7ea9af689add79f7a6ef2a3e5
Jmpuno <punojustinematthew3@gmail.com>
2026-05-29 14:40:28 +0800

    rename s3 tfstate bucket key to serverless-pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "matt-tfstate-bucket"
-     key   = "environments/dev/terraform.tfstate"
+     key   = "serverless-pipeline/dev/terraform.tfstate"
     region = "ap-southeast-1"
     encrypt = true
     use_lockfile = true
